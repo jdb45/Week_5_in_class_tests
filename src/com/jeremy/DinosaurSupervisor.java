32e40cfb311a14613d4f3e97a991ceaa04ad3364
Jeremy <acnajcadcac.yahoo.com>
2016-09-20 11:34:11 -0500

    Added comments!

--- src/com/jeremy/DinosaurSupervisor.java
+++ src/com/jeremy/DinosaurSupervisor.java
@@ -5,10 +5,13 @@ package com.jeremy;
 public class DinosaurSupervisor {
 
     public static void main(String[] args) {
+
+        //creating the dinos 
         Dinosaur t_Rex = new Dinosaur();
         Dinosaur velociraptor = new Dinosaur();
         Dinosaur diplodocus = new Dinosaur();
 
+        //having the dinos use the roar method
         t_Rex.roar();
         velociraptor.roar();
         diplodocus.roar();
